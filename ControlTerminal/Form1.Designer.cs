493b7c3e8d8d8e6ff1f0ab97180729254cba998e
becirp <44769169+becirp@users.noreply.github.com>
2020-05-20 14:43:55 +0200

    Little terminal app with console, used to read serial virtual port data.

---
+++ ControlTerminal/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ControlTerminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonRescan = new System.Windows.Forms.Button();
+            this.buttonSendCMD = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.buttonSendCMD);
+            this.groupBox1.Controls.Add(this.buttonRescan);
+            this.groupBox1.Controls.Add(this.buttonConnect);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 121);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(6, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(337, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(7, 52);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(112, 36);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonRescan
+            // 
+            this.buttonRescan.Location = new System.Drawing.Point(125, 52);
+            this.buttonRescan.Name = "buttonRescan";
+            this.buttonRescan.Size = new System.Drawing.Size(101, 36);
+            this.buttonRescan.TabIndex = 2;
+            this.buttonRescan.Text = "Rescan";
+            this.buttonRescan.UseVisualStyleBackColor = true;
+            this.buttonRescan.Click += new System.EventHandler(this.buttonRescan_Click);
+            // 
+            // buttonSendCMD
+            // 
+            this.buttonSendCMD.Location = new System.Drawing.Point(232, 52);
+            this.buttonSendCMD.Name = "buttonSendCMD";
+            this.buttonSendCMD.Size = new System.Drawing.Size(112, 36);
+            this.buttonSendCMD.TabIndex = 3;
+            this.buttonSendCMD.Text = "Get Data";
+            this.buttonSendCMD.UseVisualStyleBackColor = true;
+            this.buttonSendCMD.Click += new System.EventHandler(this.buttonSendCMD_Click);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.Location = new System.Drawing.Point(7, 19);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBoxConsole.Size = new System.Drawing.Size(336, 275);
+            this.textBoxConsole.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBoxConsole);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 342);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Console";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(242, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Auto-Read";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckStateChanged += new System.EventHandler(this.checkBox1_CheckStateChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(232, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 489);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control Terminal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonSendCMD;
+        private System.Windows.Forms.Button buttonRescan;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ControlTerminal/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ControlTerminal
+{
+    public partial class Form1 : Form
+    {
+        List<byte> input = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get all available serial ports on this PC, and put them in combobox
+            string[] ports = SerialPort.GetPortNames();
+            comboBox1.Text = "Select COM Port";
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboBox1.Items.Add(ports[i]);
+            }
+        }
+
+        public static List<byte> FormatDataToSend(string data)
+        {
+            List<byte> retVal = new List<byte>();
+            if (data == null) throw new System.ArgumentException("Parameter cannot be null", "original");
+            retVal.Add(0x1B);
+            foreach (char c in data)
+            {
+                retVal.Add(Convert.ToByte(c));
+            }
+            retVal.Add(0x00);
+            return retVal;
+        }
+
+        public void SendString(string str)
+        {
+            try
+            {
+                input = FormatDataToSend(str);
+                serialPort1.Write(input.ToArray(), 0, input.Count);
+            }
+            catch (Exception e)
+            {
+                textBoxConsole.Text += (e.Message + "\r\n");
+            }
+        }
+
+        public string CommandSendAndReceiveData(string command)
+        {
+            string ret = "";
+            bool exitf = false;
+            int tcnt = 100;
+            if (serialPort1.BytesToRead > 0) serialPort1.ReadExisting();
+            SendString(command);
+            do
+            {
+                if (serialPort1.BytesToRead > 0)
+                {
+                    ret += serialPort1.ReadExisting();
+                }
+                Thread.Sleep(10);
+                Application.DoEvents();
+                if (tcnt != 0)
+                {
+                    if (--tcnt == 0)
+                    {
+                        exitf = true;
+                    }
+                }
+            } while (exitf != true);
+            return ret;
+        }
+
+        private void ReceiveData()
+        {
+            while(serialPort1.BytesToRead > 0)
+            {
+                textBoxConsole.Text += serialPort1.ReadExisting();
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                SerialPort sp = (SerialPort)sender;
+                string indata = sp.ReadExisting();
+                BeginInvoke((MethodInvoker)delegate ()
+                {
+                    textBoxConsole.AppendText(indata + "\n");
+                });
+            }
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            //Setting up communication parameters
+            serialPort1.BaudRate = 115200;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.ReadTimeout = 500;
+            serialPort1.WriteTimeout = 500;            
+
+            if (serialPort1.IsOpen)
+            {
+                //textBoxConsole.Text += CommandSendAndReceiveData("RD"); //Send Remote Disable Command
+                serialPort1.Close();
+                buttonConnect.Text = "Connect";
+                textBoxConsole.Text += ("Disconnected from " + serialPort1.PortName + "\r\n");
+            }
+            else //If serial port closed open it and send RE command
+            {
+                String selectedCom = Convert.ToString(comboBox1.SelectedItem);
+                try
+                {
+                    serialPort1.PortName = selectedCom;
+                    serialPort1.Open();
+                    //textBoxConsole.Text += CommandSendAndReceiveData("RE"); //Send Remote Enable Command
+                    buttonConnect.Text = "Disconnect";
+                    textBoxConsole.Text += ("Connected to " + serialPort1.PortName + "\r\n");
+                }
+                catch (Exception exception1)
+                {
+                    textBoxConsole.Text += (exception1.Message + "\r\n");
+                }
+
+            }
+        }
+
+        private void buttonRescan_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+            comboBox1.Text = "Select COM Port";
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                comboBox1.Items.Add(ports[i]);
+            }
+            buttonConnect.Text = "Connect";
+        }
+
+        private void buttonSendCMD_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!checkBox1.Checked)
+                    textBoxConsole.Text += (CommandSendAndReceiveData("mRGF") + "\r\n");
+            }
+            catch (Exception exception1)
+            {
+                textBoxConsole.Text += (exception1.Message + "\r\n");
+            }
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked) serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxConsole.Clear();
+        }
+    }
+}
